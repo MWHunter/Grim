9930adb2a898c09f77baec8c2ad43cb15b47d466
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2025-01-06 16:10:32 -0700

    revert renaming TransactionOrder

--- src/main/java/ac/grim/grimac/checks/impl/misc/TransactionOrder.java
+++ src/main/java/ac/grim/grimac/checks/impl/misc/TransactionOrder.java
@@ -1,13 +1,13 @@
-package ac.grim.grimac.checks.impl.packetorder;
+package ac.grim.grimac.checks.impl.misc;
 
 import ac.grim.grimac.checks.Check;
 import ac.grim.grimac.checks.CheckData;
 import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
 
-@CheckData(name = "PacketOrderD")
-public class PacketOrderD extends Check implements PacketCheck {
-    public PacketOrderD(GrimPlayer player) {
+@CheckData(name = "TransactionOrder")
+public class TransactionOrder extends Check implements PacketCheck {
+    public TransactionOrder(GrimPlayer player) {
         super(player);
     }
 }
