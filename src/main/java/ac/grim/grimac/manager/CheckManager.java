316711f79630863f5ea413fdd1f6837fe353689b
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2025-01-06 16:12:45 -0700

    rename PacketOrderO to PacketOrderD

--- src/main/java/ac/grim/grimac/manager/CheckManager.java
+++ src/main/java/ac/grim/grimac/manager/CheckManager.java
@@ -120,7 +120,7 @@ public class CheckManager {
                 .put(PacketOrderB.class, new PacketOrderB(player))
                 .put(PacketOrderC.class, new PacketOrderC(player))
                 .put(PacketOrderD.class, new PacketOrderD(player))
-                .put(PacketOrderO.class, new PacketOrderO(player))
+                .put(PacketOrderD.class, new PacketOrderD(player))
                 .put(SprintA.class, new SprintA(player))
                 .put(VehicleA.class, new VehicleA(player))
                 .put(VehicleB.class, new VehicleB(player))
